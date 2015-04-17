ecc9b1f3ec9e715e9b2e04e103362242adb9b566
Niklas Terhoeven <niklas.terhoeven@uni-wuerzburg.de>
2015-04-17 12:43:44 +0200

    introduced Debug mode and tested with test jobs

--- slurm_hacks/run_next_job.sh
+++ slurm_hacks/run_next_job.sh
@@ -2,6 +2,7 @@
 
 echoerr() { echo "$@" 1>&2; }
 
+
 ## read options
 if [[ ! $@ ]]
 then
@@ -10,10 +11,10 @@ then
     exit 1
 fi
 
-while getopts 'j:i:n:h' OPTION ; do
+while getopts 'j:i:n:hd' OPTION ; do
     case "$OPTION" in
 	j)  JOBID=$OPTARG;;
-	i)  INTERVALL=$OPTARG;;
+	i)  INTERVAL=$OPTARG;;
 	n)  NEXT=$OPTARG;;
 	h)  echoerr "This script is used to check the slurm queue for a given JobID 
 in a given interval and runs a given command, if the job is finished.
@@ -23,8 +24,11 @@ Usage: run_next_job.sh -j <JobId> -i <Interval> -n <next command>
        JobID: Comma separated list of Job IDs to check
     Interval: Time between each check. Use number and suffix (s,m,h,d)
               default:10m
-next command: Command that should be run next (write in '')" 
+next command: Command that should be run next (write in '' and set executable)" 
 	    exit 0
+	    ;;
+	d)  echoerr "using debug mode"
+	    DEBUG=1
 	    ;;
 	*)  echoerr "Unknown parameter"
 	    echoerr "Use -h for help" 
@@ -43,13 +47,20 @@ elif [[ ! $NEXT ]]
 then
     echoerr "ERROR: no next command specified"
     exit 1
-elif [[ !$INTERVAL ]]
+elif [[ ! $INTERVAL ]]
 then
     echoerr "using default interval (10min)"
+    INTERVAL=10m
 fi
 
 
-exit
+if [[ $DEBUG ]]
+then
+echoerr "Parameters:
+JobIDs:   $JOBID
+interval: $INTERVAL
+next job: $NEXT"
+fi
 
 ## main
 RUNNING='1'
@@ -57,7 +68,14 @@ RUNNING='1'
 while [[ "$RUNNING" == "1" ]]
 do
     HIT=$(squeue -h -j $JOBID)
-    if $HIT
+    
+    if [[ $DEBUG ]]
+    then
+	date
+	echo $HIT
+    fi
+    
+    if [[ ! $HIT ]]
     then
 	RUNNING=0
     else
